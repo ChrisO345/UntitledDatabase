a183aca54ae3c562eca298820ac160caf7fb8bbd
Chris <chrisoliver345@gmail.com>
2024-07-12 18:39:20 +1200

    Working REPL

--- main.go
+++ main.go
@@ -1,9 +1,39 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strings"
 )
 
 func main() {
-	fmt.Println("Hello World")
+	fmt.Println("Untitled Database Project")
+
+	stdin := bufio.NewReader(os.Stdin)
+
+	for {
+		printPrompt()
+		input := readInput(stdin)
+
+		if input == ".exit" {
+			os.Exit(0)
+		} else {
+			fmt.Printf("Unrecognized command '%s'\n", input)
+		}
+	}
+}
+
+func printPrompt() {
+	fmt.Print("db > ")
+}
+
+func readInput(stdin *bufio.Reader) string {
+	buffer, err := stdin.ReadString('\n')
+	if err != nil {
+		fmt.Println("Error reading input")
+		os.Exit(1)
+	}
+	buffer = strings.TrimSuffix(buffer, "\r\n")
+	return buffer
 }
